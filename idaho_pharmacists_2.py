514c88932a3c442ed692454fbc933e25a1096dad
nskhwaja <44272177+nskhwaja@users.noreply.github.com>
2021-08-23 07:09:17 -0500

    Update idaho_pharmacists_2.py

--- idaho_pharmacists_2.py
+++ idaho_pharmacists_2.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 import csv
 
-url = "https://idbop.mylicense.com/verification/SearchResults.aspx"
+url = "https://idbop.mylicense.com/verification/SearchResults.aspx" #specify url
 page = requests.get(url)
 pagetext = page.text
 
@@ -13,13 +13,13 @@ soup = BeautifulSoup(pagetext, 'html.parser')
 
 file = open("test.csv", 'w')
 
-for row in soup.find_all('tr'):
+for row in soup.find_all('tr'): #html tags
     for col in row.find_all('td'):
         print(col.text)
         
   #Nested loop where I am stuck      
  import csv
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup #create csv for output
 
 outfile = open("test.csv","w",newline='')
 writer = csv.writer(outfile)
@@ -31,4 +31,4 @@ tab_data = [[item.text for item in row_data.select("tr,tbody")]
 
 for data in tab_data:
     writer.writerow(data)
-    print(' '.join(data))
+    print(' '.join(data)) #nested join where I'm stuck
